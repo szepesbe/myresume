949eb937e05852f82254fc6b97df4c46bd57b068
szepesbe <szepesbe@gmail.com>
2024-05-07 11:28:55 +0200

    skills + hobbies feltöltve

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
-            entry.target.classList.remove('show');
+          /*   entry.target.classList.remove('show'); */
         }    
     });
-},{ threshold: 0.1 });
+},{ threshold: 0.3 });
     
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.hiddenanimation');
 hiddenElements.forEach((el) => observer.observe(el));
