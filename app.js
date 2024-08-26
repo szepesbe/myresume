9d28a98c7a17939b0f491b43fb9bad4b147a4c02
szepesbe <szepesbe@gmail.com>
2024-08-26 08:31:09 +0200

    régi fájlok törlése, tevékenység animáció kialakítása

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-          /*   entry.target.classList.remove('show'); */
-        }    
+        }
     });
 },{ threshold: 0.3 });
     
 const hiddenElements = document.querySelectorAll('.hiddenanimation');
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.querySelectorAll('.image-container').forEach(function(container) {
+    container.addEventListener('click', function() {
+        let img = this.querySelector('.logo-activity');
+        img.classList.toggle('flip');
+        this.classList.toggle('flip');
+    });
+});
+
+
+
+
